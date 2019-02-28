107b8f38977e0b3a57f584bcd986f23b504a315c
rstutalo <richardtutalo@gmail.com>
2019-02-28 00:30:35 -0700

    coded all variables and switch for liri input. set up skeletons of functions being used

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
-requestAnimationFrame('dotenv').config();
+require('dotenv').config();
 
+//create variables for app functionality
+var fs = require('fs');
 var keys = require('./keys.js');
+var spotify = require('node-spotify.api');
+var inquirer = require('inquirer.js');
+var moment = require('moment.js');
+var axios = require('axios.js');
+var liriRequest = process.argv[2]
+var userInput = process.argv[3];
+
+//switches for liriRequest
+switch(liriRequest) {
+    case 'movies':
+        movieRequest();
+        break;
+    case 'artists':
+        spotifyRequest();
+        break;
+    case 'bands':
+        bandsRequest();
+        break;
+
+    // instructions for entering in information
+    default: console.log('type any of the following commands after node add.js to start the application:' + '\n' + 
+        'movies' + '\n' +
+        'artists' + '\n' +
+        'bands' + '\n');
+}
+
+// create functions to the respective apis
+
+// OMDB function
+function movieRequest() {
+    var nodeArgs = process.argv
+}
+
+function spotifyRequest() {
+
+}
+
+function bandsRequest() {
+
+}
+
